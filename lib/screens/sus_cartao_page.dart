e57999b788c6c974999933bb9457e65189b87815
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2022-03-13 04:21:49 -0300

    removendo import não utilizado

--- lib/screens/sus_cartao_page.dart
+++ lib/screens/sus_cartao_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:nossosus_app/shared/themes/app_images.dart';
-import 'package:nossosus_app/shared/themes/app_text_styles.dart';
 import 'package:nossosus_app/widgets/bottom_bar_widget.dart';
 import 'package:nossosus_app/widgets/larger_card_widget.dart';
 
